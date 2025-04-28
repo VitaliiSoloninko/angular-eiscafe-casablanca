947f14c5f3da341a81de870aaf2e0c225e7429ac
22051988Di <vitalii.soloninko@gmail.com>
2025-04-28 17:40:44 +0200

    0.3 ice 10

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../models/IProduct';
+import { TitleComponent } from '../title/title.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-products',
@@ -11,10 +11,79 @@ import { TitleComponent } from '../title/title.component';
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit {
+  price: number = 2;
+
   ice: IProduct[] = [
-    { id: 1, name: 'ice 1', description: '123', price: 200, image: '1' },
-    { id: 2, name: 'ice 2', description: '123', price: 200, image: '1' },
-    { id: 3, name: 'ice 3', description: '123', price: 200, image: '1' },
+    {
+      id: 1,
+      name: 'vanilla',
+      description: '',
+      price: this.price,
+      image: '/image/ice/vanilla.jpeg',
+    },
+    {
+      id: 2,
+      name: 'lime',
+      description: '',
+      price: this.price,
+      image: '/image/ice/lime.jpeg',
+    },
+    {
+      id: 3,
+      name: 'coconut',
+      description: '',
+      price: this.price,
+      image: '/image/ice/coconut.jpeg',
+    },
+    {
+      id: 4,
+      name: 'mint & chocolate',
+      description: '',
+      price: this.price,
+      image: '/image/ice/mint-chocolate.jpeg',
+    },
+    {
+      id: 5,
+      name: 'almond',
+      description: '',
+      price: this.price,
+      image: '/image/ice/almond.jpeg',
+    },
+    {
+      id: 6,
+      name: 'chocolate',
+      description: '',
+      price: this.price,
+      image: '/image/ice/chocolate.jpeg',
+    },
+    {
+      id: 7,
+      name: 'cream cookie',
+      description: '',
+      price: this.price,
+      image: '/image/ice/cream-cookie.jpeg',
+    },
+    {
+      id: 8,
+      name: 'blueberry',
+      description: '',
+      price: this.price,
+      image: '/image/ice/blueberry.jpeg',
+    },
+    {
+      id: 9,
+      name: 'red currant',
+      description: '',
+      price: this.price,
+      image: '/image/ice/red-currant.jpeg',
+    },
+    {
+      id: 10,
+      name: 'chocolate truffle',
+      description: '',
+      price: this.price,
+      image: '/image/ice/chocolate-truffle.jpeg',
+    },
   ];
 
   drinks: IProduct[] = [

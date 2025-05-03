835ba4bae3171a1b3563a91044b342336bab73f8
22051988Di <vitalii.soloninko@gmail.com>
2025-05-03 19:47:26 +0200

    0.12	Foods 1

---
+++ src/app/data/foods.ts
@@ -0,0 +1,60 @@
+import { IProduct } from '../models/IProduct';
+
+export const FOODS: IProduct[] = [
+  {
+    id: 1,
+    name: 'Food 1',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Food 2',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Food 3',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/3.jpg',
+  },
+  {
+    id: 4,
+    name: 'Food 4',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/4.jpg',
+  },
+  {
+    id: 5,
+    name: 'Food 5',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/5.jpg',
+  },
+  {
+    id: 6,
+    name: 'Food 6',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/6.jpg',
+  },
+  {
+    id: 7,
+    name: 'Food 7',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/7.jpg',
+  },
+  {
+    id: 8,
+    name: 'Food 8',
+    description: '',
+    price: 300 / 100,
+    image: '/image/foods/8.jpg',
+  },
+];
--- src/app/pages/food-page/food-page.component.ts
+++ src/app/pages/food-page/food-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FOODS } from '../../data/foods';
+import { IProduct } from '../../models/IProduct';
+import { ProductListComponent } from '../../products/product-list/product-list.component';
+import { TitleComponent } from '../../title/title.component';
 
 @Component({
   selector: 'app-food-page',
-  imports: [],
+  imports: [TitleComponent, ProductListComponent],
   templateUrl: './food-page.component.html',
-  styleUrl: './food-page.component.scss'
+  styleUrl: './food-page.component.scss',
 })
 export class FoodPageComponent {
-
+  foods: IProduct[] = FOODS;
 }

1ecdfcb4f73f9d977011d801f04d6d7791b0cefb
22051988Di <vitalii.soloninko@gmail.com>
2025-06-02 11:56:41 +0200

    0.21	Add modal window to deserts

--- src/app/pages/dessert-page/dessert-page.component.ts
+++ src/app/pages/dessert-page/dessert-page.component.ts
@@ -4,17 +4,32 @@ import { HOT_DRINKS, TEA } from '../../data/drinks';
 import { IProduct } from '../../models/IProduct';
 import { ProductListComponent } from '../../products/product-list/product-list.component';
 import { TitleComponent } from '../../title/title.component';
+import { ProductModalComponent } from '../../products/product-modal/product-modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dessert-page',
-  imports: [TitleComponent, ProductListComponent],
+  imports: [
+    TitleComponent,
+    ProductListComponent,
+    ProductModalComponent,
+    CommonModule,
+  ],
   templateUrl: './dessert-page.component.html',
   styleUrl: './dessert-page.component.scss',
 })
 export class DessertPageComponent {
   deserts: IProduct[] = DESERTS;
-
   hotDrinks: IProduct[] = HOT_DRINKS;
-
   tea: IProduct[] = TEA;
+
+  selectedProduct: IProduct | null = null;
+
+  openModal(product: IProduct) {
+    this.selectedProduct = product;
+  }
+
+  closeModal() {
+    this.selectedProduct = null;
+  }
 }
